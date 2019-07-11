335becbde223ea7dcce2316eada031921b97bb9d
soundreaper <soundreaper@gmail.com>
2019-07-11 15:57:36 -0700

    Add 3 new functions and new information pull
    -returns most played champion ID
    -uses champion list to return champion name
    -gets all match data information
    -add a section for most played champion within last 20
    -try to calculate winrate with champs but processing 20 matches takes a long time

--- APIGrabber.py
+++ APIGrabber.py
@@ -1,9 +1,26 @@
 import requests
 import json
 
-#Loads a file containing all champions and their respective ID numbers.
+#Loads a file containing all champions and their respective ID numbers
 with open('champions.txt') as json_file:
     champion_data = json.load(json_file)
+
+#Takes in list of last 20 champions played and returns the most played
+def currentlyMastering(List):
+    counter = 0
+    num = List[0]
+    for i in List:
+        curr_freq = List.count(i)
+        if(curr_freq > counter):
+            counter = curr_freq
+            num = i
+    return num
+
+def identifyChampion(num):
+    for x in champion_data:
+        if x['id'] == num:
+            champ = x['name']
+    return champ
 
 #Uses region, summoner name, and API Key to build URL for Summoner Info
 def requestSummonerData(region, summonerName, APIKey):
@@ -29,6 +46,12 @@ def requestMatchHistory(region, acctID, APIKey):
     response = requests.get(URL)
     return response.json()
 
+#Uses region, match ID, and API Key to build URL for match data
+def requestMatchData(region, matchID, APIKey):
+    URL = "https://" + region + "1.api.riotgames.com/lol/match/v4/matches/" + matchID + "?api_key=" + APIKey
+    response = requests.get(URL)
+    return response.json()
+
 def main():
     #Asking for user inputted region (ONLY NA AND EUW work right now)
     region = input("Enter your region: ")
@@ -48,7 +71,7 @@ def main():
 
     #Changing summoner ID and account ID to a string and assigning it to a var
     summonerID = str(responseJSON["id"])
-    accountID = str(responseJSON['accountId'])
+    #accountID = str(responseJSON['accountId'])
 
     #Calling second function to get ranked data
     responseJSON2 = requestRankedData(region, summonerID, APIKey)
@@ -57,7 +80,7 @@ def main():
     responseJSON3 = requestMasteryData(region, summonerID, APIKey)
 
     #Calling fourth function to get match history of 20 games
-    responseJSON4 = requestMatchHistory(region, accountID, APIKey)
+    #responseJSON4 = requestMatchHistory(region, accountID, APIKey)
 
     #Making sure the displayed info is for Solo Queue and not Flex Queue
     if responseJSON2[0]["queueType"] == "RANKED_SOLO_5x5":
@@ -84,9 +107,7 @@ def main():
 
     #Finding highest mastery champ and assigning mastery level & mastery points
     champID = responseJSON3[0]["championId"]
-    for x in champion_data:
-        if x['id'] == champID:
-            champName = x['name']
+    champName = identifyChampion(champID)
     masteryLevel = str(responseJSON3[0]["championLevel"])
     masteryPoints = str(responseJSON3[0]["championPoints"])
     
@@ -96,11 +117,30 @@ def main():
     print("Mastery Level: " + masteryLevel)
     print("Mastery Points: " + masteryPoints)
 
-    #
-    recentlyPlayed = responseJSON4['matches']
-    for x in recentlyPlayed:
-        print(x)
+    '''
+    #Collects list of last 20 played champions and identifies the most played
+    twentyMatchChamps = responseJSON4['matches']
+    champArr = []
+    for x in twentyMatchChamps:
+        champArr.append(x['champion'])
+    mostPlayedID = currentlyMastering(champArr)
+    champName = identifyChampion(mostPlayedID)
 
+    #Printing the champion you are currently mastering
+    print()
+    print("Currently Mastering: " + champName)
+    
+    #Calculating winrate of currently mastering champion
+    twentyMatchID = responseJSON4['matches']
+    matchIDArr = []
+    for x in twentyMatchID:
+        matchIDArr.append(x['gameId'])
+
+    for y in matchIDArr:
+        matchData = [requestMatchData(region, str(y), APIKey)]
+    
+    print(matchData[0]['teams'][0]['win'])
+    '''
 
 if __name__ == "__main__":
     main()
